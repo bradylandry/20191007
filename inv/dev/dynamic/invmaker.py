b7c1f1a270806fc44349834e6fe0b4d1c36086fa
bradylandry <brady.landry@cox.com>
2019-10-11 12:07:26 -0400

    eng of day 5

---
+++ inv/dev/dynamic/invmaker.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+iplist- []
+
+#with open("listoips.txt", "r") as listips:
+#    for line in listips:
+#        if "backbone_bbr" in line
+
+x = {'all': [{"hosts": '10.10.2.3'}, {"host": '10.10.2.4'}]}
+
+print(json.dumps("["
+
